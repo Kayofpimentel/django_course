bf2982dbf4392c9291ff919b248d999a3f6de906
Kayofpimentel <kayofpimentel@gmail.com>
2021-02-22 13:32:39 -0300

    Creating a new Journalist class Creating nested relationships with the new Journalist class and the Article class Finishing section3 example

--- src/section3/newsapi/news/api/serializers.py
+++ src/section3/newsapi/news/api/serializers.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 from django.utils.timesince import timesince
 from rest_framework import serializers as srs
-from news.models import Article
+from news.models import Article, Journalist
+# from django.db.models import fields
 
 
 class ArticleSerializer(srs.ModelSerializer):
 
     time_since_publication = srs.SerializerMethodField()
+    # author = JournalistSerializer(read_only=True)
+    # author = srs.StringRelatedField()
 
     class Meta:
         model = Article
@@ -32,45 +35,55 @@ class ArticleSerializer(srs.ModelSerializer):
             raise srs.ValidationError("The title is too short")
         return value
 
-# class ArticleSerializer(srs.Serializer):
-#     id = srs.IntegerField(read_only=True)
-#     author = srs.CharField()
-#     title = srs.CharField()
-#     description = srs.CharField()
-#     body = srs.CharField()
-#     location = srs.CharField()
-#     publication_date = srs.DateField()
-#     published = srs.BooleanField()
-#     created_at = srs.DateTimeField(read_only=True)
-#     updated_at = srs.DateTimeField(read_only=True)
 
-#     def create(_, validated_data):
-#         print(f'{validated_data:}')
-#         return Article.objects.create(**validated_data)
+class JournalistSerializer(srs.ModelSerializer):
+    articles = srs.HyperlinkedRelatedField(
+        many=True, read_only=True, view_name="article-detail")
+    # articles = ArticleSerializer(many=True, read_only=True)
 
-#     def update(self, instance, validated_data):
-#         instance.author = validated_data.get('author', instance.author)
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.description = validated_data.get(
-#             'description', instance.description)
-#         instance.body = validated_data.get('body', instance.body)
-#         instance.location = validated_data.get('location', instance.location)
-#         instance.publication_date = validated_data.get(
-#             'publication_date', instance.publication_date)
-#         instance.published = validated_data.get(
-#             'published', instance.published)
-#         instance.save()
-#         print(f'{validated_data:}')
-#         return instance
+    class Meta:
+        model = Journalist
+        fields = '__all__'
 
-#     def validate(self, data):
-#         """ Check that description and title are different """
-#         if data["title"] == data["description"]:
-#             raise srs.ValidationError(
-#                 "Title and Description must be different ")
-#         return data
+    # class ArticleSerializer(srs.Serializer):
+    #     id = srs.IntegerField(read_only=True)
+    #     author = srs.CharField()
+    #     title = srs.CharField()
+    #     description = srs.CharField()
+    #     body = srs.CharField()
+    #     location = srs.CharField()
+    #     publication_date = srs.DateField()
+    #     published = srs.BooleanField()
+    #     created_at = srs.DateTimeField(read_only=True)
+    #     updated_at = srs.DateTimeField(read_only=True)
 
-#     def validate_title(self, value):
-#         if len(value) < 60:
-#             raise srs.ValidationError("The title is too short")
-#         return value
+    #     def create(_, validated_data):
+    #         print(f'{validated_data:}')
+    #         return Article.objects.create(**validated_data)
+
+    #     def update(self, instance, validated_data):
+    #         instance.author = validated_data.get('author', instance.author)
+    #         instance.title = validated_data.get('title', instance.title)
+    #         instance.description = validated_data.get(
+    #             'description', instance.description)
+    #         instance.body = validated_data.get('body', instance.body)
+    #         instance.location = validated_data.get('location', instance.location)
+    #         instance.publication_date = validated_data.get(
+    #             'publication_date', instance.publication_date)
+    #         instance.published = validated_data.get(
+    #             'published', instance.published)
+    #         instance.save()
+    #         print(f'{validated_data:}')
+    #         return instance
+
+    #     def validate(self, data):
+    #         """ Check that description and title are different """
+    #         if data["title"] == data["description"]:
+    #             raise srs.ValidationError(
+    #                 "Title and Description must be different ")
+    #         return data
+
+    #     def validate_title(self, value):
+    #         if len(value) < 60:
+    #             raise srs.ValidationError("The title is too short")
+    #         return value
--- src/section3/newsapi/news/api/urls.py
+++ src/section3/newsapi/news/api/urls.py
@@ -1,14 +1,19 @@
 from django.conf.urls import url
 from django.urls import path
-from news.api.views import (ArticleDetailApiView, ArticleListCreateApiView)
+from news.api.views import (
+    ArticleDetailApiView, ArticleListCreateApiView, JournalistViewCreateApiView)
 # from news.api.views import (article_list_create_api_view,
 #                             article_detail_api_view)
 
 urlpatterns = [
     path("articles/", ArticleListCreateApiView.as_view(),
-         name="article_list"),
+         name="article-list"),
 
     path("article/<int:pk>", ArticleDetailApiView.as_view(),
-         name="article_detail")]
+         name="article-detail"),
+
+    path("journalists/", JournalistViewCreateApiView.as_view(),
+         name="journalist-list"), ]
+
 # path("articles/", article_list_create_api_view, name="article_list"),
 # path("article/<int:pk>", article_detail_api_view, name="article_detail")]
