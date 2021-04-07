269c6e3e3ac8532715d97b472adaca9e0ead6918
Kayofpimentel <kayofpimentel@gmail.com>
2021-04-07 14:15:00 -0300

    Creation of the questions app for the main API user interface Creation of a main HTML template for the API user access Creation of the Answer and Question models Creation of signals to create a Question unique slug Creation of the Answer and Question serializers Creation of the personalized permission class for the Question's author Creation of the Question view using DRF router Registration of Questions urls generated by the DFR router

--- src/section7/QuestionTime/QuestionTime/urls.py
+++ src/section7/QuestionTime/QuestionTime/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from core.views import IndexTemplateView
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
 from django_registration.backends.one_step.views import RegistrationView
 from users.forms import CustomUserForm
 
@@ -28,8 +29,12 @@ urlpatterns = [
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 
+    path('api/', include('users.api.urls')),
+    path('api/', include('questions.api.urls')),
 
     path('api-auth/', include('rest_framework.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
-    path('api/rest-auth/registration/', include('rest_auth.registration.urls'))
+    path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+
+    re_path(r'^.*$', IndexTemplateView.as_view(), name='entry-point')
 ]
---
+++ src/section7/QuestionTime/questions/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from questions.api.serializes import AnswerSerializer, QuestionSerializer
+from questions.models import Answer, Question
+from questions.api.permissions import IsAuthorOrReadOnly
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    lookup_field = 'slug'
+    serializer_class = QuestionSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
